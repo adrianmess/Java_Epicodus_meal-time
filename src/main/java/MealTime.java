d9ad1ebf5196643e596a80aae4c9ddf7f9487883
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 17:36:01 -0700

    prompt user, assign response to variable, respond to user including with user response incl.

--- src/main/java/MealTime.java
+++ src/main/java/MealTime.java
@@ -1,5 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MealTime {
     public static void main(String[] args){
-
+    try{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What did you eat for breakfast?");
+        String yourBreakfast = bufferedReader.readLine();
+        System.out.println("You had " + yourBreakfast + " for breakfast.");
+    }
+    catch(IOException e)
+        {
+        e.printStackTrace();
+        }
     }
 }
