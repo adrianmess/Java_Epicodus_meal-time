74aad45b0e8750b72fca78cd82627d48109f9e0f
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 17:42:23 -0700

    add prompt for lunch and dinner, add return of concatenated sentence of with lunch and dinner

--- src/main/java/MealTime.java
+++ src/main/java/MealTime.java
@@ -6,9 +6,18 @@ public class MealTime {
     public static void main(String[] args){
     try{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("What did you eat for breakfast?");
         String yourBreakfast = bufferedReader.readLine();
         System.out.println("You had " + yourBreakfast + " for breakfast.");
+
+        System.out.println("What did you eat for lunch?");
+        String yourLunch = bufferedReader.readLine();
+        System.out.println("You had " + yourLunch + " for lunch");
+
+        System.out.println("what did you eat for dinner?");
+        String yourDinner = bufferedReader.readLine();
+        System.out.println("You had" + yourDinner + " for dinner.");
     }
     catch(IOException e)
         {
