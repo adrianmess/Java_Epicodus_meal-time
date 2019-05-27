e3db2ad61465bfafdce8bfd2af227c79e64e6b23
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 18:00:11 -0700

    replace user prompts with askWhatYouAteFor method

--- src/main/java/MealTime.java
+++ src/main/java/MealTime.java
@@ -3,29 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MealTime {
-    public static void main(String[] args){
-    try{
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
 
-        System.out.println("What did you eat for breakfast?");
-        String yourBreakfast = bufferedReader.readLine();
-        System.out.println("You had " + yourBreakfast + " for breakfast.");
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("What did you eat for lunch?");
-        String yourLunch = bufferedReader.readLine();
-        System.out.println("You had " + yourLunch + " for lunch");
+            askWhatYouAteFor("breakfast");
+            String yourBreakfast = bufferedReader.readLine();
+            System.out.println("You had " + yourBreakfast + " for breakfast.");
 
-        System.out.println("what did you eat for dinner?");
-        String yourDinner = bufferedReader.readLine();
-        System.out.println("You had" + yourDinner + " for dinner.");
-    }
-    catch(IOException e)
-        {
-        e.printStackTrace();
+            askWhatYouAteFor("lunch");
+            String yourLunch = bufferedReader.readLine();
+            System.out.println("You had " + yourLunch + " for lunch");
+
+            askWhatYouAteFor("dinner");
+            String yourDinner = bufferedReader.readLine();
+            System.out.println("You had" + yourDinner + " for dinner.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
+    }
     public static void askWhatYouAteFor(String meal){
         System.out.println("What did you eat for " + meal + "?");
         }
     }
-}
+
