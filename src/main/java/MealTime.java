4a88d98240fbe136c2cb76b48241c37f14da1a25
Adrian Messado <adrian.messado@gmail.com>
2019-05-26 18:13:50 -0700

    clean up and dry-up code using askWhatYouAteFor custom method

--- src/main/java/MealTime.java
+++ src/main/java/MealTime.java
@@ -5,26 +5,20 @@ import java.io.InputStreamReader;
 public class MealTime {
     public static void main(String[] args) {
 
+        askWhatYouAteFor("breakfast");
+        askWhatYouAteFor("lunch");
+        askWhatYouAteFor("dinner");
+    }
+
+    public static void askWhatYouAteFor(String meal){
         try {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-            askWhatYouAteFor("breakfast");
-            String yourBreakfast = bufferedReader.readLine();
-            System.out.println("You had " + yourBreakfast + " for breakfast.");
-
-            askWhatYouAteFor("lunch");
-            String yourLunch = bufferedReader.readLine();
-            System.out.println("You had " + yourLunch + " for lunch");
-
-            askWhatYouAteFor("dinner");
-            String yourDinner = bufferedReader.readLine();
-            System.out.println("You had" + yourDinner + " for dinner.");
+            System.out.println("What did you eat for " + meal + "?");
+            String yourMeal = bufferedReader.readLine();
+            System.out.println("You had " + yourMeal + " for " + meal);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static void askWhatYouAteFor(String meal){
-        System.out.println("What did you eat for " + meal + "?");
-        }
-    }
+}
 
